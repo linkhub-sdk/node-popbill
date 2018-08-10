cb682199f04d48f111be3d2bbae66305325c12e7
kimhyunjin <khjzzm@gmail.com>
2018-08-10 16:14:52 +0900

    GetMessagesRN, CancelReserveRN API added and sendMessage param requestNum added

--- MessageTest.js
+++ MessageTest.js
@@ -56,22 +56,24 @@ messageService.getURL('1234567890','BOX',
     });
 
 var Messages = [{
-    Sender : '07075103710',
+    Sender : '07043042992',
     Receiver : '010111222',
     ReceiverName : '수신자명',
     Contents :'문자 메시지 내용1',
+    SenderName : "발신자명1",
 },
-    {
-        Sender : '07075103710',
-        Receiver : '010111222',
-        ReceiverName : '수신자명',
-        Contents :'문자 메시지 내용2',
-    }
+{
+    Sender : '07043042992',
+    Receiver : '010111222',
+    ReceiverName : '수신자명',
+    Contents :'문자 메시지 내용2',
+    SenderName : "발신자명2",
+}
 ]
 
 var Messages = [];
 
-messageService.sendMessage('/SMS','1234567890','07075103710','제목','동보전송 할껍니다',Messages,'',
+messageService.sendMessage('/SMS','1234567890','07043042992','제목','동보전송 할껍니다',Messages,'',
     function(response){
         console.log(response);
     },function(error){
@@ -79,46 +81,74 @@ messageService.sendMessage('/SMS','1234567890','07075103710','제목','동보전
     });
 
 
-messageService.sendSMS('1234567890', '07075103710', '010111222', '수신자명', 'SMS 단건전송', '', true,
+messageService.sendSMS('1234567890', '07043042992', '010111222', '수신자명', 'SMS 단건전송', '', true,
     function(response){
         console.log(response);
     }, function(error){
         console.log(error);
     })
 
-messageService.sendSMS_multi('1234567890', '07075103710', '동보전송을 하고싶소.', Messages,'', false,
+// requestNum
+messageService.sendSMS('1234567890', '07043042992', '010111222', '수신자명', 'SMS 단건전송', '', true, '발신자명', '20180810154037',
     function(response){
         console.log(response);
     }, function(error){
         console.log(error);
     })
 
-// LMS 단건
 
-messageService.sendLMS('1234567890', '07075103710', '010111222', '수신자명', '메시지 제목입니닷', 'LMS 단건전송', '',
+messageService.sendSMS_multi('1234567890', '07043042992', '동보전송을 하고싶소.', Messages,'', false,
     function(response){
         console.log(response);
     }, function(error){
         console.log(error);
     })
+
+
+// requestNum
+messageService.sendSMS_multi('1234567890', '07043042992', '동보전송을 하고싶소.', Messages,'', false, '20180810142432',
+    function(response){
+        console.log(response);
+    }, function(error){
+        console.log(error);
+    })
+
+
+// // LMS 단건
+messageService.sendLMS('1234567890', '07043042992', '010111222', '수신자명', '메시지 제목입니닷', 'LMS 단건전송', '20180811161016', true, '발신자명', '20180810161021',
+    function(response){
+        console.log(response);
+    }, function(error){
+        console.log(error);
+    })
+
+messageService.sendLMS('1234567890', '07043042992', '010111222', '수신자명', '메시지 제목입니닷', 'LMS 단건전송', '',
+    function(response){
+        console.log(response);
+    }, function(error){
+        console.log(error);
+    })
+
 
 var Messages = [{
-    Sender : '07075103710',
+    Sender : '07043042992',
     Receiver : '010111222',
     ReceiverName : '수신자명',
     Subject : '메시지 제목1',
-    Contents :'문자 메시지 내용1',
+    Contents :'LMS 메시지 내용1',
+    SenderName : "발신자명1",
 },
-    {
-        Sender : '07075103710',
-        Receiver : '010111222',
-        ReceiverName : '수신자명',
-        Subject : '메시지 제목2',
-        Contents :'문자 메시지 내용2MS 자동인식 단건전송 동해물과 백두산이 마르고 닳도록 하느님이 보호하사 우리나라만사 무궁화 삼천리 화려강산 ',
+{
+    Sender : '07043042992',
+    Receiver : '010111222',
+    ReceiverName : '수신자명',
+    Subject : '메시지 제목2',
+    Contents :'문자 메시지 내용2MS 자동인식 단건전송 동해물과 백두산이 마르고 닳도록 하느님이 보호하사 우리나라만사 무궁화 삼천리 화려강산 ',
+    SenderName : "발신자명2",
     }
 ]
 
-messageService.sendLMS_multi('1234567890', '07075103710', '메시지 제목', 'LMS 대량전송', Messages, '', true,
+messageService.sendLMS_multi('1234567890', '07043042992', '메시지 제목', 'LMS 대량전송', Messages, '', true,
     function(response){
         console.log(response);
     }, function(error){
@@ -126,14 +156,37 @@ messageService.sendLMS_multi('1234567890', '07075103710', '메시지 제목', 'L
     })
 
 
-messageService.sendXMS('1234567890', '07075103710', '010111222', '수신자명', '문자제목', 'XMS 자동인식 단건전송 동해물과 백두산이 마르고 닳도록 하느님이 보호하사 우리나라만사 무궁화 삼천리 화려강산 대한사람 대한으로', '', false,
+messageService.sendLMS_multi('1234567890', '07043042992', '메시지 제목', 'LMS 대량전송', Messages, '', true, "20180810155445",
     function(response){
         console.log(response);
     }, function(error){
         console.log(error);
     })
 
-messageService.sendXMS_multi('1234567890', '07075103710', '메시지 제목','자동인식전송 동보내용',Messages, '', false,
+
+messageService.sendXMS('1234567890', '07043042992', '010111222', '수신자명', '문자제목', 'XMS 자동인식 단건전송 동해물과 백두산이 마르고 닳도록 하느님이 보호하사 우리나라만사 무궁화 삼천리 화려강산 대한사람 대한으로', '', false,
+    function(response){
+        console.log(response);
+    }, function(error){
+        console.log(error);
+    })
+
+messageService.sendXMS_multi('1234567890', '07043042992', '메시지 제목','자동인식전송 동보내용',Messages, '', false,
+    function(response){
+        console.log(response);
+    }, function(error){
+        console.log(error);
+    })
+
+
+messageService.sendXMS('1234567890', '07043042992', '010111222', '수신자명', '문자제목', 'XMS 자동인식 단건전송 동해물과 백두산이 마르고 닳도록 하느님이', '', false, '발신자명', '20180810160144-01',
+    function(response){
+        console.log(response);
+    }, function(error){
+        console.log(error);
+    })
+
+messageService.sendXMS_multi('1234567890', '07043042992', '메시지 제목','자동인식전송 동보내용',Messages, '', false, '20180810155950',
     function(response){
         console.log(response);
     }, function(error){
@@ -144,14 +197,29 @@ messageService.sendXMS_multi('1234567890', '07075103710', '메시지 제목','�
 var FilePaths = ['./테스트.jpg']
 
 //MMS 단건전송
-messageService.sendMMS('1234567890', '07075103710', '01043245117','수신자명', '단건메시지제목', '단건 메시지 내용', FilePaths, '', true,
+messageService.sendMMS('1234567890', '07043042992', '010111222','수신자명', '단건메시지제목', '단건 메시지 내용', FilePaths, '', true,
     function(response){
         console.log(response);
     }, function(error){
         console.log(error);
     })
 
-messageService.sendMMS_multi('1234567890', '07075103710', '동보제목', '동보내용', Messages, FilePaths, '', false,
+messageService.sendMMS_multi('1234567890', '07043042992', '동보제목', '동보내용', Messages, FilePaths, '', false,
+    function(response){
+        console.log(response);
+    }, function(error){
+        console.log(error);
+    })
+
+
+messageService.sendMMS('1234567890', '07043042992', '010111222','수신자명', '단건메시지제목', '단건 메시지 내용', FilePaths, '', true, '20180810160258',
+    function(response){
+        console.log(response);
+    }, function(error){
+        console.log(error);
+    })
+
+messageService.sendMMS_multi('1234567890', '07043042992', '동보제목', '동보내용', Messages, FilePaths, '', false, '20180810160303',
     function(response){
         console.log(response);
     }, function(error){
@@ -165,7 +233,24 @@ messageService.getMessages('1234567890', '015081013000000005',
         console.log(error);
     })
 
+
+messageService.getMessagesRN('1234567890', '20180810161021',
+    function(response){
+        console.log(response);
+    }, function(error){
+        console.log(error);
+    })
+
+
 messageService.cancelReserve('1234567890', '015081013000000006',
+    function(response){
+        console.log(response);
+    }, function(error){
+        console.log(error);
+    });
+
+
+messageService.cancelReserveRN('1234567890', '20180810161021',
     function(response){
         console.log(response);
     }, function(error){
