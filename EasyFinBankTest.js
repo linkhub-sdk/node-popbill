0cb603d8788566a12fc5445741bf25a59cb1025c
1jae <wjkim@linkhub.co.kr>
2020-11-23 20:05:53 +0900

    added deleteBankAccount

--- EasyFinBankTest.js
+++ EasyFinBankTest.js
@@ -12,19 +12,19 @@ popbill.config({
 var easyFinBankService = popbill.EasyFinBankService();
 
 
-easyFinBankService.closeBankAccount('1234567890', '0032' ,'1122197672406', '중도',
-  function(response){
-    console.log(response);
-  }, function(error){
-    console.log(error);
-});
-
-easyFinBankService.revokeCloseBankAccount('1234567890', '0032' ,'1122197672406',
-  function(response){
-    console.log(response);
-  }, function(error){
-    console.log(error);
-});
+// easyFinBankService.closeBankAccount('1234567890', '0032' ,'1122197672406', '중도',
+//   function(response){
+//     console.log(response);
+//   }, function(error){
+//     console.log(error);
+// });
+//
+// easyFinBankService.revokeCloseBankAccount('1234567890', '0032' ,'1122197672406',
+//   function(response){
+//     console.log(response);
+//   }, function(error){
+//     console.log(error);
+// });
 
 var bankAccountInfo =  {
   BankCode : '',
@@ -36,137 +36,144 @@ var bankAccountInfo =  {
   BankID : '',
   FastID : '',
   FastPWD : '',
-  UsePeriod : '1',
+  UsePeriod : '',
   Memo : ''
 };
 
-easyFinBankService.registBankAccount('1234567890', bankAccountInfo,
+// easyFinBankService.registBankAccount('1234567890', bankAccountInfo,
+//   function(response){
+//     console.log(response);
+//   }, function(error){
+//     console.log(error);
+// });
+
+easyFinBankService.deleteBankAccount('1234567890', '0004', '1234567899870',
   function(response){
     console.log(response);
   }, function(error){
     console.log(error);
 });
 
-easyFinBankService.updateBankAccount('1234567890', bankAccountInfo,
-  function(response){
-    console.log(response);
-  }, function(error){
-    console.log(error);
-});
-
-
-easyFinBankService.getBankAccountInfo('1234567890', '0032' ,'1122197672406',
-  function(response){
-    console.log(response);
-  }, function(error){
-    console.log(error);
-});
-
-easyFinBankService.getChargeInfo('1234567890',
-  function(response){
-    console.log(response);
-  }, function(error){
-    console.log(error);
-});
-
-easyFinBankService.getChargeInfo('1234567890', 'testkorea',
-  function(response){
-    console.log(response);
-  }, function(error){
-    console.log(error);
-});
-
-easyFinBankService.getBankAccountMgtURL('1234567890',
-  function(response){
-    console.log(response);
-  }, function(error){
-    console.log(error);
-});
-
-easyFinBankService.getBankAccountMgtURL('1234567890', 'testkorea',
-  function(response){
-    console.log(response);
-  }, function(error){
-    console.log(error);
-});
-
-easyFinBankService.listBankAccount('1234567890',
-  function(response){
-    console.log(response);
-  }, function(error){
-    console.log(error);
-});
-
-
-easyFinBankService.listBankAccount('1234567890', 'testkorea',
-  function(response){
-    console.log(response);
-  }, function(error){
-    console.log(error);
-});
-
-easyFinBankService.requestJob('1234567890', '0048', '131020538645', '20191002', '20191230',
-  function(response){
-    console.log(response);
-  }, function (error){
-    console.log(error);
-});
-
-easyFinBankService.getJobState('1234567890', '019123016000000002',
-  function(response){
-    console.log(response);
-  }, function (error){
-    console.log(error);
-});
-
-
-easyFinBankService.listActiveJob('1234567890',
-  function(response){
-    console.log(response);
-  }, function (error){
-    console.log(error);
-});
-
-TradeType = ['I', 'O'];
-easyFinBankService.search('1234567890', '019123109000000004', TradeType, "", 1, 10, 'D', 'testkorea',
-  function(response){
-    console.log(response);
-  }, function(error){
-    console.log(error);
-});
-
-TradeType = ['I', 'O'];
-easyFinBankService.summary('1234567890', '019123109000000004', TradeType, "",
-  function(response){
-    console.log(response);
-  }, function(error){
-    console.log(error);
-});
-
-easyFinBankService.saveMemo('1234567890', '01912181100000000120191221000001', '메모저장-nodejs', "",
-  function(response){
-    console.log(response);
-  }, function(error){
-    console.log(error);
-});
-
-easyFinBankService.getFlatRatePopUpURL('1234567890',
-  function(response){
-    console.log(response);
-  }, function (error){
-    console.log(error);
-});
-
-easyFinBankService.getFlatRateState('1234567890', '0048', '131020538645', 'testkorea',
-  function(response){
-    console.log(response);
-  }, function (error){
-    console.log(error);
-});
-
-easyFinBankService.getChargeInfo('1234567890', 'testkorea',
-  function(response){
-    console.log(response);
-  }, function (error){
-    console.log(error);
-});
+// easyFinBankService.updateBankAccount('1234567890', bankAccountInfo,
+//   function(response){
+//     console.log(response);
+//   }, function(error){
+//     console.log(error);
+// });
+//
+//
+// easyFinBankService.getBankAccountInfo('1234567890', '0004' ,'03580204436524',
+//   function(response){
+//     console.log(response);
+//   }, function(error){
+//     console.log(error);
+// });
+//
+// easyFinBankService.getChargeInfo('1234567890',
+//   function(response){
+//     console.log(response);
+//   }, function(error){
+//     console.log(error);
+// });
+//
+// easyFinBankService.getChargeInfo('1234567890', 'testkorea',
+//   function(response){
+//     console.log(response);
+//   }, function(error){
+//     console.log(error);
+// });
+//
+// easyFinBankService.getBankAccountMgtURL('1234567890',
+//   function(response){
+//     console.log(response);
+//   }, function(error){
+//     console.log(error);
+// });
+//
+// easyFinBankService.getBankAccountMgtURL('1234567890', 'testkorea',
+//   function(response){
+//     console.log(response);
+//   }, function(error){
+//     console.log(error);
+// });
+//
+// easyFinBankService.listBankAccount('1234567890',
+//   function(response){
+//     console.log(response);
+//   }, function(error){
+//     console.log(error);
+// });
+//
+//
+// easyFinBankService.listBankAccount('1234567890', 'testkorea',
+//   function(response){
+//     console.log(response);
+//   }, function(error){
+//     console.log(error);
+// });
+//
+// easyFinBankService.requestJob('1234567890', '0048', '131020538645', '20191002', '20191230',
+//   function(response){
+//     console.log(response);
+//   }, function (error){
+//     console.log(error);
+// });
+//
+// easyFinBankService.getJobState('1234567890', '019123016000000002',
+//   function(response){
+//     console.log(response);
+//   }, function (error){
+//     console.log(error);
+// });
+//
+//
+// easyFinBankService.listActiveJob('1234567890',
+//   function(response){
+//     console.log(response);
+//   }, function (error){
+//     console.log(error);
+// });
+//
+// TradeType = ['I', 'O'];
+// easyFinBankService.search('1234567890', '019123109000000004', TradeType, "", 1, 10, 'D', 'testkorea',
+//   function(response){
+//     console.log(response);
+//   }, function(error){
+//     console.log(error);
+// });
+//
+// TradeType = ['I', 'O'];
+// easyFinBankService.summary('1234567890', '019123109000000004', TradeType, "",
+//   function(response){
+//     console.log(response);
+//   }, function(error){
+//     console.log(error);
+// });
+//
+// easyFinBankService.saveMemo('1234567890', '01912181100000000120191221000001', '메모저장-nodejs', "",
+//   function(response){
+//     console.log(response);
+//   }, function(error){
+//     console.log(error);
+// });
+//
+// easyFinBankService.getFlatRatePopUpURL('1234567890',
+//   function(response){
+//     console.log(response);
+//   }, function (error){
+//     console.log(error);
+// });
+//
+// easyFinBankService.getFlatRateState('1234567890', '0048', '131020538645', 'testkorea',
+//   function(response){
+//     console.log(response);
+//   }, function (error){
+//     console.log(error);
+// });
+//
+// easyFinBankService.getChargeInfo('1234567890', 'testkorea',
+//   function(response){
+//     console.log(response);
+//   }, function (error){
+//     console.log(error);
+// });
